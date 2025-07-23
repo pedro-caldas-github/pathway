5d31969ff28566a67e9e9af27a1463be70a87fd3
Pedro <josepedro.caldas@gmail.com>
2025-07-23 08:58:04 -0300

    finished first activity!!!

--- w4/w4_activity.py
+++ w4/w4_activity.py
@@ -1,14 +1,21 @@
-number = 10
+import random
+keep_playing = "Y"
 
-guess = -1
+while keep_playing == "Y":
+    number = random.randint(1,20)
+    guess = -1
+    guess_count = 0
 
-while guess != number:
-    guess = int(input("Guess a number between 1 and 10: "))
-    if guess < number:
-        print("Too low! Try again!")
-    elif guess > number:
-        print("Too high! Try again!")
-    else:
-        print("Congratulations, you guessed it!")
-    print("Do you want to try again? (yes/no)")
+    while guess != number:
+        guess = int(input("Guess a number between 1 and 20: "))
+        guess_count = guess_count + 1
+        if guess < number:
+            print("Too low! Try again!")
+        elif guess > number:
+            print("Too high! Try again!")
+        else:
+            print("Congratulations, you guessed it!")
+            print(f"I took you {guess_count} guesses to guess the number!")
+
+    keep_playing = input("Do you want to try again? (Y/N)")
     
